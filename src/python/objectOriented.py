94541341a06c578366bfa9206f037ddd8fbdafc3
Al <hellorisoau@gmail.com>
2024-03-08 15:17:11 +0800

    Inheritance & Polymorphism

--- src/python/objectOriented.py
+++ src/python/objectOriented.py
@@ -1,5 +1,27 @@
-# Name, ID, passkey, cardnumber
-# ID -> (Check) -> System
-# ID!=OtherID__True -> (Enter) System
-  # Features
-# ID!=OtherID__False -> (Failed Enter) System
+class Person:
+  def __init__(self, name, age) -> None:
+    self.name = name
+    self.age = age
+    
+  def getName(self):
+    return self.name
+  
+  def getAge(self):
+    return self.age
+  
+class Student(Person):
+  def __init__(self, name, age, nis) -> None:
+    super().__init__(name, age)
+    self.nis = nis
+    
+  def getNis(self):
+    return self.nis
+
+class Developer(Person):
+  def __init__(self, name, age, role) -> None:
+    super().__init__(name, age)
+    self.role = role    
+    
+  def greetings(self):
+    print(f"Welcome back {self.name}, our {self.role} good luck!")
+  
